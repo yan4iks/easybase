6b2b9a03109aff7948a6c851b2d65585b9d43805
yan4iks <janamihaleva12@gmail.com>
2025-09-08 19:23:58 +0300

    feat: Настроили админ панель с помощью наборов атрибутов

--- movies/admin.py
+++ movies/admin.py
@@ -8,6 +8,28 @@ class MovieAdmin(admin.ModelAdmin):
     list_filter = ('is_published', 'genres', 'release_year')
     search_fields = ('title', 'description')
 
+# то, что админу нельзя изменить
+    readonly_fields = ('time_create', 'time_update')
+
+# В странице админа разделяем информацию на подкатегории, fields - встроенное название
+    fieldsets = (
+        ('Основная информация',{
+            'fields': ('title', 'description', 'poster')
+        }),
+        ('Данные о релизе', {
+            'fields': (('release_year', 'rating'),) 
+            # ((___),) -в строку соединяет маленькие элементы
+        }),
+        ('Связи', {
+            'fields': ('director', 'genres')
+        }),
+        ('Статус', {
+            'classes': ('collapse',),
+            # позволяет сворачивать подкатегорию
+            'fields': ('is_published', 'time_create', 'time_update')
+        }),
+    )
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     pass
